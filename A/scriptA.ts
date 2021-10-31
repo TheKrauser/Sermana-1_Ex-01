b6e6fc9eb4156f11a2bc074e8a03c1c1f1be93e9
Kevin <kevincaireleandro@hotmail.com>
2021-10-31 01:12:41 -0300

    Corrigindo retorno e tipo das variaveis

--- A/scriptA.ts
+++ A/scriptA.ts
@@ -1,8 +1,9 @@
 function contarVogais(parametro)
 {
-    var count = parametro.match(/[aeiou]/gi); //Comando para checar na string inteira e retornar todos os resultados relacionados com o que está dentro do /[]/
-    if (count == null)
-        return 0; //Retornará o valor 0 caso a variavel count não tenha nada
+    const count = parametro.match(/[aeiou]/gi); //Comando para checar na string inteira e retornar todos os resultados relacionados com o que está dentro do /[]/
+   
+    if (!count)
+        return 0; //Retornará o valor 0 caso a variavel count não tenha nada (seja ela Undefined, Null, False ou 0)
     else
         return count; //Se houver um valor então retorna ele
 }
