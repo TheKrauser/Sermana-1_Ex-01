ce3968d64c02faf9bc5ebe5b6286c0da4fb252c7
Kevin <kevincaireleandro@hotmail.com>
2021-11-02 11:53:10 -0300

    Acentos e Conversão para Letra Minúscula

--- A/scriptA.ts
+++ A/scriptA.ts
@@ -1,6 +1,7 @@
 function contarVogais(parametro : string) : number
 {
-    const count = parametro.match(/[aeiou]/gi); //Comando para checar na string inteira e retornar todos os resultados relacionados com o que está dentro do /[]/
+    let minusculo = parametro.toLowerCase(); //Mudar toda a string para letra minúscula
+    const count = minusculo.match(/[aáàãâeéêiíîoóõôuúû]/gi); //Comando para checar na string inteira e retornar todos os resultados relacionados com o que está dentro do /[]/
    
     if (!count)
         return 0; //Retornará o valor 0 caso a variavel count não tenha nada (seja ela Undefined, Null, False ou 0)
@@ -8,6 +9,7 @@ function contarVogais(parametro : string) : number
         return count.length; //Se houver um valor então retorna ele, sendo a quantidade de vogais encontradas na palavra/frase
 }
 
-console.log(contarVogais("Testando"));
+console.log(contarVogais("TesTandO"));
 console.log(contarVogais("Kevin"));
 console.log(contarVogais("Vamos testar com uma frase"));
+console.log(contarVogais("TeStAnDíNhÔ"));
--- scripts/scriptA.js
+++ scripts/scriptA.js
@@ -1,11 +1,13 @@
 function contarVogais(parametro) {
-    var count = parametro.match(/[aeiou]/gi); //Comando para checar na string inteira e retornar todos os resultados relacionados com o que está dentro do /[]/
+    var minusculo = parametro.toLowerCase(); //Mudar toda a string para letra minúscula
+    var count = minusculo.match(/[aáàãâeéêiíîoóõôuúû]/gi); //Comando para checar na string inteira e retornar todos os resultados relacionados com o que está dentro do /[]/
     if (!count)
         return 0; //Retornará o valor 0 caso a variavel count não tenha nada (seja ela Undefined, Null, False ou 0)
     else
-        return count.length; //Se houver um valor então retorna ele
+        return count.length; //Se houver um valor então retorna ele, sendo a quantidade de vogais encontradas na palavra/frase
 }
-console.log(contarVogais("Testando"));
+console.log(contarVogais("TesTandO"));
 console.log(contarVogais("Kevin"));
 console.log(contarVogais("Vamos testar com uma frase"));
+console.log(contarVogais("TeStAnDíNhÔ"));
 //# sourceMappingURL=scriptA.js.map
